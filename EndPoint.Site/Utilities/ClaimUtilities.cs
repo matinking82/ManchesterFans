63af56e021bcd4f9f80a0704294e60dc2b02c76a
Matin Babaki <matinking82@gmail.com>
2020-11-21 11:07:52 +0330

    Add ClaimUtilities

---
+++ EndPoint.Site/Utilities/ClaimUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EndPoint.Site.Utilities
+{
+    public static class ClaimUtilities
+    {
+        public static int? GetUserId(this ClaimsPrincipal User)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return int.Parse(((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value);
+            }
+            return null;
+        }
+    }
+}
